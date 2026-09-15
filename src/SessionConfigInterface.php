bb4d1f9ebcecafee3dcf1cde3226315ea8c30093
S.Tougeiro <sidneytougeiro@gmail.com>
2026-09-15 16:50:44 +0000

    Refactor SessionConfigInterface: update method signatures and return types for clarity

--- src/SessionConfigInterface.php
+++ src/SessionConfigInterface.php
@@ -7,21 +7,45 @@
     {
         /** @return string
          */
+        public function handler(): string;
+
+        /** @return string
+         */
         public function name(): string;
-
-        /** @return array<string, mixed>
-         */
-        public function cookie(): array;
-
-        /** @return array<string, mixed>
-         */
-        public function gc(): array;
-
-        /** @return array<string, mixed>
-         */
-        public function extra(): array;
 
         /** @return string
          */
         public function storage(): string;
+
+        /** @return int
+         */
+        public function cookieLifetime(): int;
+
+        /** @return string
+         */
+        public function cookieSameSite(): string;
+
+        /** @return int
+         */
+        public function gcMaxLifetime(): int;
+
+        /** @return int
+         */
+        public function gcProbability(): int;
+
+        /** @return int
+         */
+        public function gcDivisor(): int;
+
+        /** @return bool
+         */
+        public function regeneration(): bool;
+
+        /** @return int
+         */
+        public function regenerationTime(): int;
+
+        /** @return array<string, mixed>
+         */
+        public function extra(): array;
     }
